a3dde6a9d5ca1a51ead68f3c4c7246754410fe80
LTSFben <ltsf.benja@gmail.com>
2021-10-05 11:45:05 -0300

    props + states examples

--- src/components/Greet.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Greet = () => <h1>Welcome {sessionStorage.getItem("user-name")}</h1>;
-
-export default Greet;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Greet from "./Components/Greet.js";
-import Message from "./Components/Message.js";
-import Table from "./Components/Table.js";
-import Cat from "./Components/Cat.js";
-import LifeCycle from "./Components/LifeCycle.js";
-import Click from "./Components/Click.js";
-import Bind from "./Components/Bind.js";
 
-sessionStorage.setItem("user-name", "Benjamin");
+import Header from "./Components/Header.js";
+import Button from "./Components/Button.js";
 
-class Index extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <div>
-        <Greet />
-        <Message />
-        <Table id="1" firstName="Benjamin" lastName="Pla" email="benjaminpla.dev@gmail.com" />
-        <Cat catName="Felix" eyeColor="Yellow" age="4" />
-        <LifeCycle />
-        <Click />
-        <Bind />
+        <Header text="Hello world" />
+        <Button />
       </div>
     );
   }
 }
 
-ReactDOM.render(<Index />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
