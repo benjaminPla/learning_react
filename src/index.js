ffb3c09418b8465dc86b808c7b749e04ec2fd0e4
benjaminPla <benjaminpla.dev@gmail.com>
2021-10-19 18:23:04 -0300

    cleaning space to work

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Header } from "./Components/Header.js";
-import Button from "./Components/Button.js";
-import Toggle from "./Components/Hooks/useState.js";
-import InputEvent from "./Components/InputEvent.js";
-import UseStateArray from "./Components/Hooks/useStateArray.js";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header text="Hello world" />
-        <Button text="Change humor" />
-        <Toggle />
-        <InputEvent />
-        <UseStateArray />
-      </div>
-    );
-  }
+function App() {
+  return <></>;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
