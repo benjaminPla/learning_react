8099c29e5047d4c6ddadf7e668a104f80655d71b
benjaminPla <benjaminpla.dev@gmail.com>
2021-10-21 14:24:49 -0300

    add COnditionalFromProps.jsx example

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import IfElse from "./components/IfElse.jsx";
+import CheckUserAge from "./components/CheckUserAge.jsx";
+import ConditionalFromProps from "./components/ConditionalFromPorps.jsx";
 
 function App() {
   return (
     <>
       <IfElse />
+      <CheckUserAge />
+      <ConditionalFromProps />
     </>
   );
 }
